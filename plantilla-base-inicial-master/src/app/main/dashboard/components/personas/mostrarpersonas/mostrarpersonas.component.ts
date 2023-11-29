169095a8fad93a2f1c3858329867f6f666a413c2
Mayk913 <lomejor913@gmail.com>
2023-11-28 21:08:26 -0500

    implementando el mostrar personas

--- plantilla-base-inicial-master/src/app/main/dashboard/components/personas/mostrarpersonas/mostrarpersonas.component.ts
+++ plantilla-base-inicial-master/src/app/main/dashboard/components/personas/mostrarpersonas/mostrarpersonas.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/core/services/usuarios/user.service';
+import { PersonI } from 'src/app/models/user/person';
 
 @Component({
   selector: 'app-mostrarpersonas',
   templateUrl: './mostrarpersonas.component.html',
   styleUrls: ['./mostrarpersonas.component.css']
 })
-export class MostrarpersonasComponent {
+export class MostrarpersonasComponent implements OnInit {
+  public personas: PersonI[] = [];
+  public displayedColumns: string[] = ["id", "name", "surname", "address", "phone", "user", "nationality", "date_of_birth", "Acciones"];
 
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.mostrarPersonas();
+  }
+
+  mostrarPersonas() {
+    this.userService.getPeople().subscribe({
+      next: (data) => {
+        this.personas = data;
+        console.log(this.personas);
+      }
+    });
+  }
 }
