e3bb85a46f239057707442e00569d76f2bac7d8b
Mayk913 <lomejor913@gmail.com>
2023-12-02 07:18:50 -0500

    creadno boton de crear personas a la vista person

--- plantilla-base-inicial-master/src/app/main/dashboard/components/personas/mostrarpersonas/mostrarpersonas.component.ts
+++ plantilla-base-inicial-master/src/app/main/dashboard/components/personas/mostrarpersonas/mostrarpersonas.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/usuarios/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DialogService } from 'primeng/dynamicdialog';
 import { PersonI } from 'src/app/models/user/person';
 
 @Component({
@@ -10,8 +12,30 @@ import { PersonI } from 'src/app/models/user/person';
 export class MostrarpersonasComponent implements OnInit {
   public personas: PersonI[] = [];
   public displayedColumns: string[] = ["id", "name", "surname","document_type" ,"identification","date_of_birth","address", "phone_number", "user", "nationality", "date_of_birth", "Acciones"];
+  public Dialog = false;
 
-  constructor(private userService: UserService) { }
+  public form:FormGroup=this.formBuilder.group({
+    id: [''],
+    name: ['', [Validators.required]],
+    surname: ['', [Validators.required]],
+    DocumentTypeId: ['', [Validators.required]],
+    identification: ['', [Validators.required]],
+    GenderId: ['', [Validators.required]],
+    // address:['', [Validators.required]],
+    phone: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    nationality: ['', [Validators.required]],
+    date_of_birth: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+      //password2: ['', [Validators.required]],
+    documentTypeId: ['', [Validators.required]],
+
+  });
+
+  constructor(
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    ) { }
 
   ngOnInit(): void {
     this.mostrarPersonas();
@@ -25,4 +49,12 @@ export class MostrarpersonasComponent implements OnInit {
       }
     });
   }
+  openDialog() {
+    this.Dialog = true;
+  }
+
+  onSubmit(){
+
+  }
+
 }
